ef88261106ce459999420ac870bd11102b0f3379
Adrian Haasler García <dev@adrianhaasler.com>
2015-05-01 17:03:31 +0200

    Add category generator
    Obtained from https://github.com/hexojs/hexo-generator-category. It will be
    extended to support multilingual content.

---
+++ scripts/generators.js
@@ -0,0 +1,23 @@
+var pagination = require('hexo-pagination');
+
+hexo.extend.generator.register('category', function(locals){
+  var config = this.config;
+  var perPage = config.category_generator.per_page;
+  var paginationDir = config.pagination_dir || 'page';
+
+  return locals.categories.reduce(function(result, category){
+    if (!category.length) return result;
+
+    var posts = category.posts.sort('-date');
+    var data = pagination(category.path, posts, {
+      perPage: perPage,
+      layout: ['category', 'archive', 'index'],
+      format: paginationDir + '/%d/',
+      data: {
+        category: category.name
+      }
+    });
+
+    return result.concat(data);
+  }, []);
+});
