77f962375801b70d11f1f187fc1b9a36a8019e4b
undefined <dev@adrianhaasler.com>
2015-05-03 08:09:42 +0000

    Adapt category generator to multilingual content
    The category info will be obtained from a data file located at the
    `source/_data` folder of the main Hexo project (not inside the theme). The file
    must be named `categories.yml` (or .json) and should contain something like
    this:
    
    ```
    news:
      en:
        name: News
        slug: news
        category: news
      es:
        name: Noticias
        slug: noticias
        category: noticias
    ```
    
    Built from 5a7c9c808ca7f6408bba1b3742a49c8b238e56e8.

--- scripts/generators.js
+++ scripts/generators.js
@@ -1,27 +1,34 @@
 var pagination = require('hexo-pagination');
+var _ = require('lodash');
 
 hexo.extend.generator.register('category', function(locals){
   var config = this.config;
+  var categories = locals.data.categories;
   if (config.category_generator) {
     var perPage = config.category_generator.per_page;
   } else {
     var perPage = 10;
   }
   var paginationDir = config.pagination_dir || 'page';
+  var result = [];
 
-  return locals.categories.reduce(function(result, category){
-    if (!category.length) return result;
-
-    var posts = category.posts.sort('-date');
-    var data = pagination(category.path, posts, {
-      perPage: perPage,
-      layout: ['category', 'archive', 'index'],
-      format: paginationDir + '/%d/',
-      data: {
-        category: category.name
-      }
+  _.each(categories, function(data, title) {
+    _.each(data, function(data, lang) {
+      result = result.concat({
+        path: lang + '/' + data.slug + '/index.html',
+        data: {
+          posts: getCategoryByName(locals.categories, data.category).posts
+        },
+        layout: ['archive', 'index']
+      });
     });
+  });
 
-    return result.concat(data);
-  }, []);
+  return result;
 });
+
+function getCategoryByName(categories, name) {
+  return categories.toArray().filter(function(category){
+    return category.name == name;
+  })[0];
+}
