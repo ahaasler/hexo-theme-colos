837196ade0510c59f72bb1c2aee7f2d2238dbdce
undefined <dev@adrianhaasler.com>
2015-06-28 19:59:26 +0000

    Add function to retrieve nested items from objects
    Obtained from http://stackoverflow.com/a/16190716.
    
    Built from 3c0ae66a30b8a70d92e5d9e460f96bbacc3f8294.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -42,3 +42,20 @@ hexo.extend.helper.register('_c', function(string) {
 	}
 	return this.config[string];
 });
+
+/**
+ * Retrieve nested item from object/array (http://stackoverflow.com/a/16190716)
+ * @param {Object|Array} obj
+ * @param {String} path dot separated
+ * @param {*} def default value ( if result undefined )
+ * @returns {*}
+ */
+function path(obj, path, def) {
+	var i, len;
+	for (i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+		if (!obj || typeof obj !== 'object') return def;
+		obj = obj[path[i]];
+	}
+	if (obj === undefined) return def;
+	return obj;
+}
