a024728e1b6b1ccb0280df493a414e8c584dce0f
Adrian Haasler García <dev@adrianhaasler.com>
2015-07-20 16:39:33 +0000

    Remove is_post helper
    Since the layouts can now easily check if a page is a post the helper is
    not necessary anymore. Relates to #1.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -4,23 +4,6 @@ var regexCache = {
 	post: {}
 };
 
-hexo.extend.helper.register('is_post', function() {
-	var permalink = this.config.permalink;
-	var r = regexCache.post[permalink];
-	if (!r) {
-		var rUrl = permalink
-			.replace(':lang', '[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*')
-			.replace(':id', '\\d+')
-			.replace(':category', '(\\w+\\/?)+')
-			.replace(':year', '\\d{4}')
-			.replace(/:(month|day)/g, '\\d{2}')
-			.replace(/:i_(month|day)/g, '\\d{1,2}')
-			.replace(/:title/, '[^\\/]+');
-		r = regexCache.post[permalink] = new RegExp(rUrl);
-	}
-	return r.test(this.path);
-});
-
 hexo.extend.helper.register('get_categories_for_lang', function(lang) {
 	var result = [];
 	_.each(this.site.data.categories, function(category, title) {
