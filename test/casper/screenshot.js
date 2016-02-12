a21a82bcdc64752cee279eff182f69e9cf411f75
Adrian Haasler García <dev@adrianhaasler.com>
2016-02-12 17:57:42 +0000

    Wait for the web components to load
    This should fix #19.

--- test/casper/screenshot.js
+++ test/casper/screenshot.js
@@ -6,7 +6,9 @@ casper.options.viewportSize = {
 casper.test.begin('Take screenshot', 0, function suite(test) {
 
 	casper.start('http://localhost:8000/hexo-theme-colos', function() {
-		casper.capture('dist/theme/screenshot.png');
+		casper.wait(1500, function() {
+			casper.capture('dist/theme/screenshot.png');
+		});
 	});
 
 	casper.run(function() {
