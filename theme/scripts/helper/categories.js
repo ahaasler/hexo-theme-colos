19ed6d6eb401fd2debbb5778272a528fdcc44d77
Adrian Haasler García <dev@adrianhaasler.com>
2016-04-26 19:11:01 +0000

    Return list instead of HTML in getCategories

--- theme/scripts/helper/categories.js
+++ theme/scripts/helper/categories.js
@@ -2,13 +2,13 @@
 
 hexo.extend.helper.register('header_menu', function headerCategoriesHelper() {
   var categories = getCategories(this, arguments[0]);
-  var result = categories.html;
-  result = '<style is="custom-style">paper-tab[link] a { @apply(--layout-horizontal); @apply(--layout-center-center); }</style>' + result;
-  result = result.replace(new RegExp("<ul ", 'g'), "<paper-tabs ");
-  result = result.replace(new RegExp("</ul>", 'g'), "</paper-tabs>");
-  result = result.replace(new RegExp("<li ", 'g'), "<paper-tab link ");
-  result = result.replace(new RegExp("</li>", 'g'), "</paper-tab>");
-  if (categories.current > 0) {
+  var result = '<style is="custom-style">paper-tab[link] a { @apply(--layout-horizontal); @apply(--layout-center-center); }</style>';
+  result += '<paper-tabs class="category-list">';
+  for (var i = 0; i < categories.categories.length; i++) {
+    result += '<paper-tab link class="category-list-item"><a class="category-list-link" href="' + categories.categories[i].url + '">' + categories.categories[i].name + '</a></paper-tab>';
+  }
+  result += '</paper-tabs>';
+  if (categories.current >= 0) {
     result = result.replace(new RegExp("<paper-tabs "), '<paper-tabs selected="' + categories.current + '" ');
   }
   return result;
@@ -24,20 +24,25 @@ hexo.extend.helper.register('list_categories_drawer', function drawerCategoriesH
 });
 
 function getCategories(self, config) {
-  var categories = {
-    html: self.list_categories(config),
+  var html = self.list_categories(config);
+  var result = {
+    categories: [],
     current: -1
   };
-  // Get current
-  var currentIndex = categories.html.search(/<li [^>]*><a [^>]*class="[^"]*current[^"]*"[^>]*>/i);
-  if (currentIndex > 0) {
-    for (var pos = categories.html.search(/<li /), last = -1, selected = 0; pos !== -1; pos = categories.html.substr(last + 1).search(/<li /), selected++) {
-      last += pos + 1;
-      if (last === currentIndex) {
-        categories.current = selected;
-        break;
-      }
+  var pattern = /<li [^>]*><a [^>]*class="([^"]*)"[^>]*href="([^"]*)"[^>]*>([^a]*)<\/a><\/li>/gi;
+  var match;
+  var i = 0;
+  while (match = pattern.exec(html)) {
+    var current = match[1].indexOf('current') >= 0;
+    if (current) {
+      result.current = i;
     }
+    result.categories.push({
+      name: match[3],
+      url: match[2],
+      current: current
+    });
+    i++;
   }
-  return categories;
+  return result;
 }
