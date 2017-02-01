6c209bef7800c12135f3cda1f24d58b104ed49df
Adrian Haasler García <dev@adrianhaasler.com>
2017-02-01 18:15:51 +0000

    Set screenshot size to fix #45

--- test/casper/screenshot.js
+++ test/casper/screenshot.js
@@ -1,4 +1,4 @@
-casper.options.viewportSize = {
+var viewport = {
 	width: 800,
 	height: 500
 };
@@ -7,8 +7,9 @@ casper.test.begin('Take screenshot', 0, function suite(test) {
 
 	casper.start('http://localhost:8000/hexo-theme-colos', function() {
 		casper.zoom(0.6);
+		casper.viewport(viewport.width, viewport.height);
 		casper.wait(2500, function() {
-			casper.capture('dist/theme/screenshot.png');
+			casper.capture('dist/theme/screenshot.png', {top: 0, left: 0, width: viewport.width, height: viewport.height});
 		});
 	});
 
