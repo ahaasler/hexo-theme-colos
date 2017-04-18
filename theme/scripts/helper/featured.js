b8093e0f6270bc2cbc2de9b47e6bb3050a7b7fa5
Adrian Haasler García <dev@adrianhaasler.com>
2017-04-18 18:15:05 +0000

    Add support for multilingual featured posts
    This fixes #50.

--- theme/scripts/helper/featured.js
+++ theme/scripts/helper/featured.js
@@ -1,7 +1,8 @@
 'use strict';
 
 hexo.extend.helper.register('featured_posts', function featuredPosts() {
+  var self = this;
   return this.site.posts.sort('date').filter(function(post) {
-    return post.featured === true;
+    return post.featured === true && (self.config.multilingual === false || self.page.lang === post.lang);
   });
 });
