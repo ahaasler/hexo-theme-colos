7a9dd85f6c60cff01c50a4b92af24323bdabcca8
Adrian Haasler García <dev@adrianhaasler.com>
2017-06-24 18:36:48 +0000

    Add helper to display social links
    Built from 18252d04d9a1507ff52190be1f09baca79e30e6e.

---
+++ scripts/helper/social.js
@@ -0,0 +1,26 @@
+'use strict';
+
+hexo.extend.helper.register('list_social', function listSocial() {
+  var result = '';
+  var social = this.theme.social || {};
+  for (var type in social) {
+    if (social.hasOwnProperty(type)) {
+      result += '<a href="' + this.url_for_social(type) + '" alt="' + type + '"><iron-icon icon="social-iconset:' + type + '"></iron-icon></a>'
+    }
+  }
+  return result;
+});
+
+hexo.extend.helper.register('url_for_social', function urlForSocial(type) {
+  var value = this.theme.social[type];
+  switch (type) {
+    case 'github':
+      return 'https://github.com/' + value;
+    case 'googleplus':
+      return 'https://plus.google.com/' + value;
+    case 'twitter':
+      return 'https://twitter.com/' + value;
+    default:
+      return '#';
+  }
+});
